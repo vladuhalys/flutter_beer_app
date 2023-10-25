9221386b45df42b01dcd98d8d1dc2133e6312396
Lysenko Vladyslav <vlad.lysenko1999@gmail.com>
2023-10-25 16:19:53 +0300

    Update models in data

--- lib/features/beer_list/data/models/beer/boil_volume.dart
+++ lib/features/beer_list/data/models/beer/boil_volume.dart
@@ -1,7 +1,20 @@
-import 'volume.dart';
+import 'package:flutter_beer_app/features/beer_list/domain/entities/beer/boil_volume.dart';
 
-class BoilVolume extends Volume {
-  BoilVolume({int? value, String? unit}) : super(value: value, unit: unit);
+class BoilVolumeModel extends BoilVolume {
+  const BoilVolumeModel({int? value, String? unit})
+      : super(value: value, unit: unit);
 
-  BoilVolume.fromJson(Map<String, dynamic> json) : super.fromJson(json);
+  factory BoilVolumeModel.fromJson(Map<String, dynamic> json) {
+    return BoilVolumeModel(
+      value: json['value'],
+      unit: json['unit'],
+    );
+  }
+
+  factory BoilVolumeModel.fromEntity(BoilVolume entity) {
+    return BoilVolumeModel(
+      value: entity.value,
+      unit: entity.unit,
+    );
+  }
 }
--- lib/features/beer_list/data/models/beer/volume.dart
+++ lib/features/beer_list/data/models/beer/volume.dart
@@ -1,11 +1,20 @@
-class Volume {
-  int? value;
-  String? unit;
+import 'package:flutter_beer_app/features/beer_list/domain/entities/beer/volume.dart';
 
-  Volume({this.value, this.unit});
+class VolumeModel extends Volume {
+  const VolumeModel({int? value, String? unit})
+      : super(value: value, unit: unit);
 
-  Volume.fromJson(Map<String, dynamic> json) {
-    value = json['value'];
-    unit = json['unit'];
+  factory VolumeModel.fromJson(Map<String, dynamic> json) {
+    return VolumeModel(
+      value: json['value'],
+      unit: json['unit'],
+    );
+  }
+
+  factory VolumeModel.fromEntity(Volume entity) {
+    return VolumeModel(
+      value: entity.value,
+      unit: entity.unit,
+    );
   }
 }
